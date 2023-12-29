8f41c90e8c859763865e944d3ebef730a4c7f716
Leul Wujira <meraf000@gmail.com>
2023-12-29 23:08:10 +0300

    feat: add folder structure and webpack configuration

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const sayHello = () => {
+  alert('Hello World');
+};
+
+document.getElementById('button').addEventListener('click', sayHello);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+const outputPath = 'dist';
+const entryPoints = {
+  main: [path.resolve(__dirname, 'src', 'main.ts')],
+  background: path.resolve(__dirname, 'src', 'background.ts'),
+};
+
+module.exports = {
+  entry: entryPoints,
+  output: {
+    path: path.join(__dirname, outputPath),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)$/i,
+        use: 'url-loader?limit=1024',
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '.', to: '.', context: 'public' }],
+    }),
+  ],
+};
